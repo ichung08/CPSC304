09315af3e2d37540cfefb4fc3e71ca4793d8b237
ichung08 <ichung08@students.cs.ubc.ca>
2023-04-05 04:09:17 -0700

    implemented delete

--- frontend/src/components/TournamentTable.js
+++ frontend/src/components/TournamentTable.js
@@ -27,7 +27,20 @@ const Td = styled.td`
   border: 1px solid #ccc;
 `;
 
-const TournamentTable = ({ tournament }) => {
+const Button = styled.button`
+  background-color: #f44336;
+  border: none;
+  color: white;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+`;
+
+const TournamentTable = ({ tournament, handleDelete }) => {
   return (
     <Table>
       <thead>
@@ -37,6 +50,7 @@ const TournamentTable = ({ tournament }) => {
           <Th>Start Date</Th>
           <Th>End Date</Th>
           <Th>Prize Pool</Th>
+          <Th></Th>
         </Tr>
       </thead>
       <tbody>
@@ -47,6 +61,9 @@ const TournamentTable = ({ tournament }) => {
             <Td>{tournament.startdate.substring(0, 10)}</Td>
             <Td>{tournament.enddate.substring(0, 10)}</Td>
             <Td>{tournament.prize_pool}</Td>
+            <Td>
+              <Button onClick={() => handleDelete(tournament.tournament_id)}>Delete</Button>
+            </Td>
           </Tr>
         ))}
       </tbody>
